24dffc3346ba42b8b7e597c6ab4f8110047c3308
Leonidas810 <leonardo20010912@gmail.com>
2026-09-06 15:33:36 -0600

    update/smallest stable index II

--- Smallest Stable index I/main.py
+++ Smallest Stable index I/main.py
@@ -1,10 +1,31 @@
-#First aproach
+#First Approach
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         for i in range(len(nums)):
             currentStable = max(nums[0:i+1]) - min(nums[i:])
             
             if currentStable <= k:
+                return i
+        
+        return -1
+
+#Second Approach
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        suffix_min = [0] * n
+        suffix_min[-1] = nums[-1]
+
+        for i in range(n-2,-1,-1):
+            suffix_min[i] = min(nums[i],suffix_min[i+1])
+        
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max,nums[i])
+
+            if prefix_max - suffix_min[i] <= k:
                 return i
         
         return -1
